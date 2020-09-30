37c3e3201e2878fcfaa272e545cb23f2f9495e01
marchmin <462887806@qq.com>
2020-09-30 16:18:53 +0800

    Update diy.sh

--- k2p/diy.sh
+++ k2p/diy.sh
@@ -1,3 +1,3 @@
 rm -Rf files/etc/profile.d/sysinfo.sh
-find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+find target/linux/ar71xx/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/tree/master/target/linux/ar71xx target/linux/ar71xx
